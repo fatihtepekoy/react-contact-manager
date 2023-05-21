edb8e658605474835f7c3f80d5edbe8f6532221c
fatih <fatiht@shipmercury.com>
2023-05-21 21:48:52 +0300

    edit contact - have bugs

--- src/App.js
+++ src/App.js
@@ -2,23 +2,30 @@ import './styles.css';
 import Header from './Header';
 import ContactForm from './ContactForm';
 import Contacts from './Contacts';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 
 
 function App() {
 
-  const firstContact = [
-    {
-      id: 1,
-      firstName: "Fatih",
-      lastName: "Tepekoy",
-      phoneNumber: "5332109104"
-    }
-  ]
+  var firstContact =
+  {
+    id: 1,
+    firstName: "Fatih",
+    lastName: "Tepekoy",
+    phoneNumber: "5332109104"
+  }
 
-  const [contacts, setContacts] = useState(firstContact);
+
+
+  // Run! Like go get some data from an API.
+  const [contacts, setContacts] = useState([firstContact]);
+
+  // useEffect(() => {
+  //   setContacts(firstContact);
+  // }, []);
+
 
 
 
@@ -26,7 +33,7 @@ function App() {
     <div>
       <Header />
       <ContactForm contacts={contacts} setContacts={setContacts} />
-      <Contacts contacts={contacts} />
+      <Contacts contacts={contacts} setContacts={setContacts} />
     </div>
   )
 }
--- src/Contacts.js
+++ src/Contacts.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import ContactTable from './ContactTable';
@@ -6,17 +6,39 @@ import ContactTable from './ContactTable';
 
 
 
-const Contacts = ({ contacts }) => {
+const Contacts = ({ contacts, setContacts }) => {
+
 
   const [show, setShow] = useState(false);
   const [selectedContactId, setSelectedContactId] = useState(1);
 
   const selectedContact = contacts[selectedContactId - 1];
-  console.log(selectedContact.firstName);
-
+  let updatedContacts;
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const saveContact = (event) => {
+    const contact =
+    {
+      id: selectedContactId,
+      firstName: event.target.firstName.value,
+      lastName: event.target.lastName.value,
+      phoneNumber: event.target.phoneNumber.value
+    }
+
+    updatedContacts = contacts.map(item => {
+      if (item.id === selectedContactId) {
+        return contact;
+      }
+      return item;
+    });
+
+
+    setContacts(updatedContacts);
+
+  }
+
 
   const getEditModal = () => {
     return (
@@ -30,13 +52,26 @@ const Contacts = ({ contacts }) => {
           <Modal.Title>Edit Contact</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          {selectedContact.firstName}
+          <form onSubmit={saveContact} id="contact-form">
+            <label>
+              First Name:
+              <input type="text" name="firstName" defaultValue={selectedContact.firstName} />
+            </label>
+            <label>
+              Last name:
+              <input type="text" name="lastName" defaultValue={selectedContact.lastName} />
+            </label>
+            <label>
+              Phone Number:
+              <input type="number" name="phoneNumber" defaultValue={selectedContact.phoneNumber} />
+            </label>
+          </form>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary">Understood</Button>
+          <Button type="submit" form="contact-form" variant="primary">Save</Button>
         </Modal.Footer>
       </Modal>
     );
@@ -44,19 +79,23 @@ const Contacts = ({ contacts }) => {
 
   const editModal = getEditModal();
 
-
-  if (show) {
-    return (
-      <>
+  function getContactPart() {
+    if (show) {
+      return (
+        <>
+          <ContactTable contacts={contacts} setShow={setShow} setSelectedContactId={setSelectedContactId} />
+          <div>{editModal}</div>
+        </>
+      )
+    } else {
+      return (
         <ContactTable contacts={contacts} setShow={setShow} setSelectedContactId={setSelectedContactId} />
-        <div>{editModal}</div>
-      </>
-    )
-  } else {
-    return (
-      <ContactTable contacts={contacts} setShow={setShow} setSelectedContactId={setSelectedContactId} />
-    )
+      )
+    }
   }
+
+  return contacts.length > 0 ? getContactPart() : null;
+
 
 }
 
